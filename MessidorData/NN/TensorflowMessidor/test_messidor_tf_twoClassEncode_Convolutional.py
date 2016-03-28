b32fb3a0e3ecd7d160ac160dbcb1b4297be446e3
devrimgl <istanbul1330>
2016-03-28 18:29:44 +0100

    Convolutional updated

--- MessidorData/NN/TensorflowMessidor/test_messidor_tf_twoClassEncode_Convolutional.py
+++ MessidorData/NN/TensorflowMessidor/test_messidor_tf_twoClassEncode_Convolutional.py
@@ -18,17 +18,18 @@ def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides= [1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 #400 10MB data
-#DATA_DIRECTORY_PATH = '/Users/macbookair/Dropbox/image-eye/test'
-#data_file_path = '/Users/macbookair/Dropbox/image-eye/test/AnnotationBaseTest1.csv'
-#TRAIN_DATA_SIZE = 350
+DATA_DIRECTORY_PATH = '/Users/macbookair/Dropbox/image-eye/test'
+data_file_path = '/Users/macbookair/Dropbox/image-eye/test/AnnotationBaseTest1.csv'
+TRAIN_DATA_SIZE = 350
 
 #1200 mix data
-DATA_DIRECTORY_PATH = '/Users/macbookair/Dropbox/image-eye/data'
-data_file_path = '/Users/macbookair/Dropbox/image-eye/data/data.csv'
-TRAIN_DATA_SIZE = 1000
+#DATA_DIRECTORY_PATH = '/Users/macbookair/Dropbox/image-eye/data'
+#data_file_path = '/Users/macbookair/Dropbox/image-eye/data/data.csv'
+#TRAIN_DATA_SIZE = 1000
+
 sess = tf.InteractiveSession()
 
 #image
@@ -57,30 +58,28 @@ data_sets.test = tce.DataSet(test_images, test_labels)
 x = tf.placeholder(tf.float32, [None, IMAGE_SIZE])
 y_ = tf.placeholder(tf.float32, [None, 2])
 
-W_conv1 = weight_variable([5,5,3,4])
-b_conv1 = weight_variable([4])
+W_conv1 = weight_variable([5,5,3,32])
+b_conv1 = weight_variable([32])
 x_image = tf.reshape(x, [-1, IMAGE_D1, IMAGE_D2, IMAGE_D3])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
-W_fc1 = weight_variable([140*93*3*4, 512])
+W_fc1 = weight_variable([140*93*32, 512])
 b_fc1 = bias_variable([512])
 
-h_pool_flat = tf.reshape(h_pool1, [-1,140*93*3*4])
+h_pool_flat = tf.reshape(h_pool1, [-1,140*93*32])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool_flat, W_fc1) + b_fc1)
 
-keep_prob = tf.placeholder('float')
-h_fc1_dropout = tf.nn.dropout(h_fc1, keep_prob)
 
 W_fc2 = weight_variable([512,2])
 b_fc2 = bias_variable([2])
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_dropout, W_fc2) + b_fc2)
+y_conv = tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2)
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.initialize_all_variables())
 for i in range(50):
     batch = data_sets.train.next_batch(50)
-    train_step.run(feed_dict={x: data_sets.train.images, y_: data_sets.train.labels})
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
 print "test accuracy %g" % accuracy.eval(feed_dict={x: data_sets.test.images, y_: data_sets.test.labels})
