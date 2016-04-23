k-fold: 6-fold 1000training 200 test bug fix: cross entropy nan problem, solution: using different optimizers with smaller learning rate blur cv2 removed, histogram normalization added(need to check again) 9 convolutional, 6 pooling, 1 dropout, 2 fully connected layer tried 256, BOM! max image resolution 128, batch 50, range 5000

MessidorData/NN/TensorflowMessidor/messidor_tf_twoClassEncode.py
@@ -9,6 +9,7 @@ import numpy as np
 import settings
 import cv2
 from collections import OrderedDict
+import operator
 
 # (DR1, DR2, DR3) and DR0
 labels_file_path = settings.dataFilePath
@@ -53,6 +54,20 @@ def read_image_file_names(image_file_path):
         image_data.close()
     return image_list'''
 
+def equalize(im):
+    h = im.convert("L").histogram()
+    lut = []
+    for b in range(0, len(h), 256):
+        # step size
+        step = reduce(operator.add, h[b:b+256]) /255
+        # create equalization lookup table
+        n = 0
+        for i in range(256):
+            lut.append(n / step)
+            n = n+ h[i+b]
+    # map image through lookup table
+    return im.point(lut * 3)
+
 def read_labels_and_image_names(labels_file_path):
     image_list = dict()
     image_data = open(labels_file_path, 'r')
@@ -73,6 +88,7 @@ def read_labels_and_image_names(labels_file_path):
     labels = temp.values()
     return names, np.asarray(labels)
 
+
 def create_images_arrays(image_list, data_directory_path):
     """
     It reads all image files and created a list of image arrays,
@@ -84,13 +100,15 @@ def create_images_arrays(image_list, data_directory_path):
     for image in image_list:
         image_path = os.path.join(data_directory_path, image)
         im = Image.open(image_path)
+        im = equalize(im)
+        # im = Image.open(image_path).convert("L")
         im.thumbnail((IMAGE_D1, IMAGE_D2), Image.ANTIALIAS)
         im = np.array(im, dtype=np.float32)
-        b = np.zeros(im.shape)
+        '''b = np.zeros(im.shape)
         cv2.circle(b, (im.shape[1] / 2, im.shape[0] / 2), int(IMAGE_D1 * 0.9), (1, 1, 1), -1, 8, 0)
         im_blur = cv2.addWeighted(im, 4, cv2.GaussianBlur(im, (0, 0), IMAGE_D1 / 30), -4, 128) * b + 128 * (1 - b)
-        imarray = np.array(im_blur, dtype=np.float32)
-        images.append(imarray)
+        imarray = np.array(im_blur, dtype=np.float32)'''
+        images.append(im)
     gc.collect()
     return np.array(images, dtype=np.float32)
 
@@ -108,7 +126,7 @@ class DataSet(object):
       # to [num examples, rows*columns] (assuming depth == 1)
       # assert images.shape[3] == 1
       images = images.reshape(images.shape[0],
-                              images.shape[1] * images.shape[2]*images.shape[3])
+                              images.shape[1] * images.shape[2] * images.shape[3])
       # Convert from [0, 255] -> [0.0, 1.0].
       images = images.astype(np.float32)
       images = np.multiply(images, 1.0 / 255.0)

MessidorData/NN/TensorflowMessidor/test_messidor_tf_twoClassEncode.py
@@ -12,7 +12,7 @@ def weight_variable(shape):
     return tf.Variable(initial)
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
+    initial = tf.constant(0.0, shape=shape)
     return tf.Variable(initial)
 
 
@@ -59,7 +59,7 @@ cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 cross_entropy = tf.Print(cross_entropy, [cross_entropy], "CrossE")
 
 # train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-5).minimize(cross_entropy)
 
 # initialize the variables we created:
 init = tf.initialize_all_variables()
@@ -68,7 +68,7 @@ init = tf.initialize_all_variables()
 sess = tf.InteractiveSession()
 sess.run(init)
 
-# Model Evalution
+# Model Evaluation
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 
 # Kaci dogru kaci yanlis karsilastirmasi
@@ -78,9 +78,10 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 for i in range(RANGE):
     print ('Iteration', i)
     batch = data_sets.train.next_batch(BATCH)
-    train_accuracy = accuracy.eval(feed_dict= {
-        x: batch[0], y_: batch[1]})
-    print "step %d, training accuracy %g" %(i, train_accuracy)
+    if i% 10 == 0:
+        train_accuracy = accuracy.eval(feed_dict= {
+            x: data_sets.train.images, y_: data_sets.train.labels})
+        print "step %d, training accuracy %g" %(i, train_accuracy)
     train_step.run(feed_dict={x: data_sets.train.images, y_: data_sets.train.labels})
 
     #sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
@@ -90,5 +91,5 @@ for i in range(RANGE):
 # print(sess.run(accuracy, feed_dict={x: data_sets.test.images, y_: data_sets.test.labels}))
 print "test accuracy %g" % accuracy.eval(feed_dict={x: data_sets.test.images, y_: data_sets.test.labels})
 
-probability = y
-print probability.eval(feed_dict={x:data_sets.test.images}, session=sess)
+# probability = y
+# print probability.eval(feed_dict={x:data_sets.test.images}, session=sess)

MessidorData/NN/TensorflowMessidor/test_messidor_tf_twoClassEncode_Convolutional.py
@@ -3,34 +3,40 @@ import tensorflow as tf
 import numpy as np
 from sklearn import cross_validation
 import settings
-
+from sklearn.cross_validation import KFold
 
 class DataSets(object):
     pass
 
+
 def weight_variable(shape):
-    # initial = tf.truncated_normal(shape, stddev=0.1)
-    initial = tf.random_uniform(shape, minval=0, maxval=None, dtype=tf.float32,seed=1)
+    initial = tf.truncated_normal(shape, stddev=0.1, seed=23)
+    # initial = tf.random_uniform(shape, minval=0, maxval=None, dtype=tf.float32,seed=1)
     return tf.Variable(initial)
+
 
 def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
+
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides= [1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')  # strides changed to 1
+
 
 data_directory_path = settings.dataDirectoryPath
 data_file_path = settings.dataFilePath
 TRAIN_DATA_SIZE = settings.trainDataSize
-
+KERNEL_SIZE = settings.kernelSize
+LAYER_CONSTANT = settings.layerPoolConstant
 
 sess = tf.InteractiveSession()
 
-#image
+# image
 IMAGE_D1 = settings.imageDimension1
 IMAGE_D2 = settings.imageDimension2
 IMAGE_D3 = settings.imageDimension3
@@ -47,79 +53,127 @@ print('Reading dataset..')
 file_names, labels = tce.read_labels_and_image_names(data_file_path)
 
 images = tce.create_images_arrays(file_names, data_directory_path)
+kf = KFold(len(images), n_folds=6)
 
-train_images = images[:TRAIN_DATA_SIZE]
-test_images = images[TRAIN_DATA_SIZE:]
-train_labels = labels[:TRAIN_DATA_SIZE]
-test_labels = labels[TRAIN_DATA_SIZE:]
+acc = []
+for train_index, test_index in kf:
+    # train_images = images[:TRAIN_DATA_SIZE]
+    # test_images = images[TRAIN_DATA_SIZE:]
+    # train_labels = labels[:TRAIN_DATA_SIZE]
+    # test_labels = labels[TRAIN_DATA_SIZE:]
+    train_images = images[train_index]
+    test_images = images[test_index]
+    train_labels = labels[train_index]
+    test_labels = labels[test_index]
 
+    data_sets = DataSets()
+    data_sets.train = tce.DataSet(train_images, train_labels)
+    data_sets.test = tce.DataSet(test_images, test_labels)
+    print("Data set is loaded..")
+    x = tf.placeholder(tf.float32, [None, IMAGE_SIZE])
+    y_ = tf.placeholder(tf.float32, [None, 2])
 
-data_sets = DataSets()
-data_sets.train = tce.DataSet(train_images, train_labels)
-data_sets.test = tce.DataSet(test_images, test_labels)
-print("Data set is loaded..")
-x = tf.placeholder(tf.float32, [None, IMAGE_SIZE])
-y_ = tf.placeholder(tf.float32, [None, 2])
+    W_conv1 = weight_variable([KERNEL_SIZE, KERNEL_SIZE, IMAGE_D3, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
+    b_conv1 = weight_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
+    x_image = tf.reshape(x, [-1, IMAGE_D1, IMAGE_D2, IMAGE_D3])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
-W_conv1 = weight_variable([5,5,3,IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
-b_conv1 = weight_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
-x_image = tf.reshape(x, [-1, IMAGE_D1, IMAGE_D2, IMAGE_D3])
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+    W_conv2 = weight_variable(
+        [KERNEL_SIZE, KERNEL_SIZE, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
+    b_conv2 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
 
-W_conv2 = weight_variable([5, 5, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
-b_conv2 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
+    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+    h_pool1 = max_pool_2x2(h_conv2)
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-#devrim lalalal
-W_conv3 = weight_variable([5, 5, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
-b_conv3 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
+    W_conv3 = weight_variable(
+        [KERNEL_SIZE, KERNEL_SIZE, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 2])
+    b_conv3 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 2])
 
-h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
-h_pool3 = max_pool_2x2(h_conv3)
+    h_conv3 = tf.nn.relu(conv2d(h_pool1, W_conv3) + b_conv3)
 
+    W_conv4 = weight_variable(
+        [KERNEL_SIZE, KERNEL_SIZE, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 2, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 2])
+    b_conv4 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 2])
 
-#reklamlar bitti
+    h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4) + b_conv4)
+    h_pool2 = max_pool_2x2(h_conv4)
 
+    W_conv5 = weight_variable(
+        [KERNEL_SIZE, KERNEL_SIZE, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 2, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 4])
+    b_conv5 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 4])
 
-W_fc1 = weight_variable([(IMAGE_D1/8)*(IMAGE_D2/8)*IMAGE_CONVOLUTIONAL_LAYER_OUTPUT, IMAGE_DENSELY_CONNECTED_LAYER_OUTPUT])
-b_fc1 = bias_variable([IMAGE_DENSELY_CONNECTED_LAYER_OUTPUT])
+    h_conv5 = tf.nn.relu(conv2d(h_pool2, W_conv5) + b_conv5)
 
-h_pool_flat = tf.reshape(h_pool3, [-1,(IMAGE_D1/8)*(IMAGE_D2/8)*IMAGE_CONVOLUTIONAL_LAYER_OUTPUT])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool_flat, W_fc1) + b_fc1)
+    W_conv6 = weight_variable(
+        [KERNEL_SIZE, KERNEL_SIZE, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 4, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 4])
+    b_conv6 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 4])
 
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    h_conv6 = tf.nn.relu(conv2d(h_conv5, W_conv6) + b_conv6)
+    h_pool3 = max_pool_2x2(h_conv6)
 
-W_fc2 = weight_variable([IMAGE_DENSELY_CONNECTED_LAYER_OUTPUT,2])
-b_fc2 = bias_variable([2])
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    W_conv7 = weight_variable(
+        [KERNEL_SIZE, KERNEL_SIZE, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 4, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 8])
+    b_conv7 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 8])
 
+    h_conv7 = tf.nn.relu(conv2d(h_pool3, W_conv7) + b_conv7)
+    h_pool4 = max_pool_2x2(h_conv7)
 
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
-cross_entropy = tf.Print(cross_entropy, [cross_entropy], "CrossE")
+    W_conv8 = weight_variable(
+        [KERNEL_SIZE, KERNEL_SIZE, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 8, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 8])
+    b_conv8 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 8])
 
+    h_conv8 = tf.nn.relu(conv2d(h_pool4, W_conv8) + b_conv8)
+    h_pool5 = max_pool_2x2(h_conv8)
 
-train_step = tf.train.AdamOptimizer(1e-5).minimize(cross_entropy)
+    W_conv9 = weight_variable(
+        [KERNEL_SIZE, KERNEL_SIZE, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 8, IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 16])
+    b_conv9 = bias_variable([IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 16])
 
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print("initializing all variables")
-sess.run(tf.initialize_all_variables())
-for i in range(RANGE):
-    print ("iteration : ", i)
-    batch = data_sets.train.next_batch(BATCH)
-    train_accuracy = accuracy.eval(feed_dict={
-        x: batch[0], y_: batch[1], keep_prob:1.0})
-    print "step %d, training accuracy %g" % (i, train_accuracy)
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob:0.5})
+    h_conv9 = tf.nn.relu(conv2d(h_pool5, W_conv9) + b_conv9)
+    h_pool6 = max_pool_2x2(h_conv9)
 
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_pool6, keep_prob)
 
-#print "test accuracy %g" % accuracy.eval(feed_dict={x: data_sets.test.images, y_: data_sets.test.labels})
+    W_fc1 = weight_variable(
+        [(IMAGE_D1 / LAYER_CONSTANT) * (IMAGE_D2 / LAYER_CONSTANT) * IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 16,
+         IMAGE_DENSELY_CONNECTED_LAYER_OUTPUT])
+    b_fc1 = bias_variable([IMAGE_DENSELY_CONNECTED_LAYER_OUTPUT])
 
-feed_dict={x: data_sets.test.images, keep_prob:1.0}
-prediction = tf.argmax(y_conv, 1)
-print prediction.eval(feed_dict=feed_dict, session=sess)
-probabilities = y_conv
-print probabilities.eval(feed_dict=feed_dict, session=sess)
+    h_pool_flat = tf.reshape(h_pool6, [-1, (IMAGE_D1 / LAYER_CONSTANT) * (
+        IMAGE_D2 / LAYER_CONSTANT) * IMAGE_CONVOLUTIONAL_LAYER_OUTPUT * 16])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool_flat, W_fc1) + b_fc1)
+
+    W_fc2 = weight_variable([IMAGE_DENSELY_CONNECTED_LAYER_OUTPUT, 2])
+    b_fc2 = bias_variable([2])
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2)
+
+    cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
+    cross_entropy = tf.Print(cross_entropy, [cross_entropy], "CrossE")
+    train_step = tf.train.AdamOptimizer(1e-5).minimize(cross_entropy)
+
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print("initializing all variables")
+    sess.run(tf.initialize_all_variables())
+    for i in range(RANGE):
+        print ("iteration : ", i)
+        batch = data_sets.train.next_batch(BATCH)
+        if i % 1000 == 0:
+         train_accuracy = accuracy.eval(feed_dict={
+             x: data_sets.train.images, y_: data_sets.train.labels, keep_prob: 0.5})
+         print "step %d, training accuracy %g" % (i, train_accuracy)
+        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+    test_acc = accuracy.eval(
+        feed_dict={x: data_sets.test.images, y_: data_sets.test.labels, keep_prob: 0.5})
+    print "test accuracy %g" % test_acc
+    acc.append(test_acc)
+
+print('Average Test Accuracy: ', sum(acc)/ len(acc))
+
+    # feed_dict = {x: data_sets.test.images, keep_prob: 0.5}
+    # prediction = tf.argmax(y_conv, 1)
+    # print prediction.eval(feed_dict=feed_dict, session=sess)
+    # probabilities = y_conv
+    # print probabilities.eval(feed_dict=feed_dict, session=sess)